48653d4deaceb9568c8340afe3698d47a4a01e7d
Mai Truong <maitruong2801@gmail.com>
2019-01-26 08:06:39 -0800

    add the main Python file to do prediction. It is following a tutorial on Datacamp for the moment but I will make changes as neccessary to fit the project

---
+++ stock_predict.py
@@ -0,0 +1,9 @@
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
