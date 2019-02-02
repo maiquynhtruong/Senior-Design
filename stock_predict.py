0f352327078f1933611aa008d8725c3462bb5009
Mai Truong <maitruong2801@gmail.com>
2019-02-02 13:48:48 -0500

    Done set up LSTMs

--- stock_predict.py
+++ stock_predict.py
@@ -115,11 +115,176 @@ for pred_idx in range(1, N):
 
 print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
 
-plt.figure(figsize=(18,9))
-plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
-plt.plot(range(0, N), run_avg_predictions, color='orange', label='Prediction')
+# plt.figure(figsize=(18,9))
+# plt.plot(range(df.shape[0]), all_mid_data, color='b', label='True')
+# plt.plot(range(0, N), run_avg_predictions, color='orange', label='Prediction')
 # plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
-plt.xlabel('Date')
-plt.ylabel('Mid Price (High + Low)/2')
-plt.legend(fontsize=18)
-plt.show()
+# plt.xlabel('Date')
+# plt.ylabel('Mid Price (High + Low)/2')
+# plt.legend(fontsize=18)
+# plt.show()
+
+################### Stock prediction with LSTM ###################
+
+class DataGeneratorSeq(object):
+    def __init__(self, prices, batch_size, num_unroll):
+        self._prices = prices
+        self._prices_length = len(self._prices) - num_unroll
+        self._batch_size = batch_size
+        self._num_unroll = num_unroll
+        self._segments = self._prices_length // self._batch_size
+        self._cursor = [offset * self._segments for offset in range(self._batch_size)]
+
+    def next_batch(self):
+        batch_data = np.zeros(self._batch_size, dtype=np.float32)
+        batch_labels = np.zeros(self._batch_size, dtype=np.float32)
+
+        for b in range(self._batch_size):
+            if self._cursor[b]+1 >= self._prices_length:
+                self._cursor[b] = np.random.randint(0, (b+1)*self._segments)
+
+            batch_data[b] = self._prices[self._cursor[b]]
+            batch_labels[b] = self._prices[self._cursor[b]+np.random.randint(0,5)]
+
+            self._cursor[b] = (self._cursor[b]+1) * self._prices_length
+
+        return batch_data, batch_labels
+
+    def unroll_batches(self):
+        unroll_data, unroll_labels = [], []
+        init_data, init_label = None, None
+        for ui in range(self._num_unroll):
+            data, label = self.next_batch()
+            unroll_data.append(data)
+            unroll_labels.append(label)
+
+        return unroll_data, unroll_labels
+
+    def reset_indices(self):
+        for b in range(self._batch_size):
+            self._cursor[b] = np.random.randint(0, min((b+1)*self._segments, self._prices_length-1))
+
+data_gen = DataGeneratorSeq(train_data, 5, 5)
+u_data, u_labels = data_gen.unroll_batches()
+
+for ui,(data,label) in enumerate(zip(u_data, u_labels)):
+    print('\n\nUnrolled index %d'%ui)
+    data_ind = data
+    label_ind = label
+    print('\tInputs: ', data)
+    print('\tOutputs: ', label)
+
+## Defining hyperparameters
+
+D = 1 # Dimensionality of the input, i.e. stock price
+num_unrollings = 50 # How many contnuous time steps for a single optimization step
+batch_size = 500 # number of samples in a batch
+num_nodes = [200, 200, 150] # number of hidden nodes in each layer
+n_layers = len(num_nodes) # number of layers
+dropout = 0.5 # dropout amount
+
+tf.reset_default_graph()
+
+## Defining inputs and outputs
+
+train_inputs, train_outputs = [], []
+
+# Unroll the input over time defining placeholders for each time step
+for ui in range(num_unrollings):
+    train_inputs.append(tf.placeholder(tf.float32, shape=[batch_size,D], name='train_inputs_%d'%ui))
+    train_outputs.append(tf.placeholder(tf.float32, shape=[batch_size,1], name='train_outputs_%d'%ui))
+
+## Defining parameters of the LSTM and the regression layers
+
+lstm_cells = [tf.contrib.rnn.LSTMCell(num_units=num_nodes[li],
+                                    state_is_tuple=True,
+                                    initializer=tf.contrib.layers.xavier_initializer())
+    for li in range(n_layers)]
+drop_lstm_cells = [tf.contrib.rnn.DropoutWrapper(cell=lstm,
+                                                input_keep_prob=1.0,
+                                                output_keep_prob=1.0-dropout,
+                                                state_keep_prob=1.0-dropout
+) for lstm in lstm_cells]
+
+drop_multi_cell = tf.contrib.rnn.MultiRNNCell(drop_lstm_cells)
+multi_cell = tf.contrib.rnn.MultiRNNCell(lstm_cells)
+
+w = tf.get_variable(name='w', shape=[num_nodes[-1],1], initializer=tf.contrib.layers.xavier_initializer())
+b = tf.get_variable(name='b', initializer=tf.random_uniform(shape=[1], minval=-0.1, maxval=0.1))
+
+## Calculating LSTM output and Feeding it to the regression layer to get final prediction
+
+c, h = [], [] # # cell state and hidden state variables to maintain the state of the LSTM
+initial_state = []
+for li in range(n_layers):
+    c.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    h.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+    initial_state.append(tf.contrib.rnn.LSTMStateTuple(c[li], h[li]))
+
+# Do several tensor transofmations, because the function dynamic_rnn requires the output to be of
+# a specific format. Read more at: https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn
+all_inputs = tf.concat([tf.expand_dims(t,0) for t in train_inputs], axis=0)
+
+# all_outputs is [seq_length, batch_size, num_nodes]
+all_lstm_outputs, state = tf.nn.dynamic_rnn(drop_multi_cell, all_inputs,
+                            initial_state=tuple(initial_state),
+                            time_major=True, dtype=tf.float32)
+
+all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings, num_nodes[-1]])
+
+all_outputs = tf.nn.xw_plus_b(all_lstm_outputs, w, b) #calculate the LSTM outputs with the tf.nn.dynamic_rnn function
+split_outputs = tf.split(value=all_outputs, num_or_size_splits=num_unrollings, axis=0) #  split the output back to a list of num_unrolling tensors
+
+## Loss calculation and optimizer
+
+# When calculating the loss you need to be careful about the exact form, because you calculate
+# loss of all the unrolled steps at the same time
+# Therefore, take the mean error or each batch and get the sum of that over all the unrolled steps
+print('Defining training loss')
+loss = 0.0
+with tf.control_dependencies([tf.assign(c[li], state[li][0]) for li in range(n_layers)]+
+                             [tf.assign(h[li], state[li][1]) for li in range(n_layers)]):
+    for ui in range(num_unrollings):
+        loss += tf.reduce_mean(0.5*(split_outputs[ui]-train_outputs[li])**2)
+
+print('Learning rate decay operation')
+global_step = tf.Variable(0, trainable=False)
+inc_gstep = tf.assign(global_step, global_step+1)
+tf_learning_rate = tf.placeholder(shape=None, dtype=tf.float32)
+tf_min_learning_rate = tf.placeholder(shape=None, dtype=tf.float32)
+
+learning_rate = tf.maximum(
+    tf.train.exponential_decay(tf_learning_rate, global_step, decay_steps=1, decay_rate=0.5, staircase=True), tf_min_learning_rate
+)
+
+# Optimizer
+print('Tf optimizer operation')
+optimizer = tf.train.AdamOptimizer(learning_rate)
+gradients, v = zip(*optimizer.compute_gradients(loss))
+gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+optimizer = optimizer.apply_gradients(zip(gradients, v))
+print('\tAll done')
+
+## Prediction Related Calculations
+print('Defining prediction related to TF functions')
+sample_inputs = tf.placeholder(tf.float32, shape=[1,D])
+
+# Maintaining LSTM stage for prediction stage
+sample_c, sample_h, initial_sample_state = [], [], []
+for li in range(n_layers):
+    sample_c.append(tf.Variable(tf.zeros([1, num_nodes[li]]), trainable=False))
+    sample_h.append(tf.Variable(tf.zeros([1, num_nodes[li]]), trainable=False))
+    initial_sample_state.append(tf.contrib.rnn.LSTMStateTuple(sample_c[li], sample_h[li]))
+
+reset_sample_states = tf.group(*[tf.assign(sample_c[li], tf.zeros([1, num_nodes[li]])) for li in range(n_layers)],
+                               *[tf.assign(sample_h[li], tf.zeros([1, num_nodes[li]])) for li in range(n_layers)])
+
+sample_outputs, sample_state = tf.nn.dynamic_rnn(multi_cell, tf.expand_dims(sample_inputs, 0),
+                                                initial_state=tuple(initial_sample_state),
+                                                time_major=True,
+                                                dtype=tf.float32)
+
+with tf.control_dependencies([tf.assign(sample_c[li], sample_state[li][0]) for li in range(n_layers)] +
+                             [tf.assign(sample_h[li], sample_state[li][1]) for li in range(n_layers)]):
+    sample_prediction = tf.nn.xw_plus_b(tf.reshape(sample_outputs, [1,-1]), w, b)
+print('\tAll done')
