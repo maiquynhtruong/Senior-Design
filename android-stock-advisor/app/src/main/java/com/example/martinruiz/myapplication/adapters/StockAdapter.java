8f49adf7b40b16453f1508a5fc87c92c54a7cb03
Mai Truong <maitruong2801@gmail.com>
2019-02-07 12:48:50 -0500

    add Stock Adapter

--- android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/adapters/StockAdapter.java
+++ android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/adapters/StockAdapter.java
@@ -1,4 +1,39 @@
 package com.example.martinruiz.myapplication.adapters;
 
-public class StockAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.martinruiz.myapplication.models.Stock;
+
+import butterknife.ButterKnife;
+
+public class StockAdapter extends RecyclerView.Adapter<StockAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bind(Stock stock) {
+            
+        }
+    }
 }
