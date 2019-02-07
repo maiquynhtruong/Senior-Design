bee1b6289c9fb74ff559163699919d8b9bc60def
Mai Truong <maitruong2801@gmail.com>
2019-02-06 23:39:45 -0500

    add Stock class

---
+++ android-weather-app-master/app/src/main/java/com/example/martinruiz/myapplication/models/Stock.java
@@ -0,0 +1,26 @@
+package com.example.martinruiz.myapplication.models;
+
+import java.io.Serializable;
+
+public class Stock implements Serializable {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    String name;
+    Double price;
+    Double predictedPrice;
+
+}
