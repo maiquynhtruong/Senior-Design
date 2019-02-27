89efc733e1e67e9c65ed387388ac52b74a661325
Mai Truong <maitruong2801@gmail.com>
2019-02-26 23:14:42 -0500

    add graphing code

--- trainer/task.py
+++ trainer/task.py
@@ -430,3 +430,35 @@ for ep in range(epochs):
         print('\tTest MSE: %.5f'%np.mean(mse_test_loss_seq))
         predictions_over_time.append(predictions_seq)
         print('\tFinished Predictions')
+
+best_prediction_epoch = 28 # replace this with the epoch that you got the best results when running the plotting code
+
+plt.figure(figsize = (18,18))
+plt.subplot(2,1,1)
+plt.plot(range(df.shape[0]),all_mid_data,color='b')
+
+# Plotting how the predictions change over time
+# Plot older predictions with low alpha and newer predictions with high alpha
+start_alpha = 0.25
+alpha  = np.arange(start_alpha,1.1,(1.0-start_alpha)/len(predictions_over_time[::3]))
+for p_i,p in enumerate(predictions_over_time[::3]):
+    for xval,yval in zip(x_axis_seq,p):
+        plt.plot(xval,yval,color='r',alpha=alpha[p_i])
+
+plt.title('Evolution of Test Predictions Over Time',fontsize=18)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.xlim(train_test_split,12500)
+
+plt.subplot(2,1,2)
+
+# Predicting the best test prediction you got
+plt.plot(range(df.shape[0]),all_mid_data,color='b')
+for xval,yval in zip(x_axis_seq,predictions_over_time[best_prediction_epoch]):
+    plt.plot(xval,yval,color='r')
+
+plt.title('Best Test Predictions Over Time',fontsize=18)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
+plt.xlim(train_test_split,12500)
+plt.show()
