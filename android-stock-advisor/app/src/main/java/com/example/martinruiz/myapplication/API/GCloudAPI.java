87aa4231e4f645633027625c24e6ec8f85fa72b0
Mai Truong <maitruong2801@gmail.com>
2019-04-02 17:50:56 -0400

    add a few ideas for credentials

--- android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/API/GCloudAPI.java
+++ android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/API/GCloudAPI.java
@@ -18,6 +18,8 @@ package com.example.martinruiz.myapplication.API;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.martinruiz.myapplication.R;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.GenericUrl;
@@ -28,10 +30,22 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.UriTemplate;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.IOUtils;
+import com.google.api.gax.paging.Page;
 import com.google.api.services.discovery.Discovery;
 import com.google.api.services.discovery.model.JsonSchema;
 import com.google.api.services.discovery.model.RestDescription;
 import com.google.api.services.discovery.model.RestMethod;
+import com.google.auth.oauth2.ComputeEngineCredentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Collections;
 
 /*
  * Sample code for doing Cloud Machine Learning Engine online prediction in Java.
@@ -55,33 +69,46 @@ public class GCloudAPI extends AsyncTask<String, Integer, String> {
 
 
     protected String doInBackground(String... stockNames) {
-        HttpTransport httpTransport = new com.google.api.client.http.javanet.NetHttpTransport();
+        HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+//        HttpTransport httpTransport = new com.google.api.client.http.javanet.NetHttpTransport();
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        Discovery discovery = new Discovery.Builder(httpTransport, jsonFactory, null).build();
-
+        GoogleCredentials credentials = ComputeEngineCredentials.create();
         String stockName = stockNames[0];
 
         try {
-            RestDescription api = discovery.apis().getRest("ml", "v1").execute();
-            RestMethod method = api.getResources().get("projects").getMethods().get("predict");
+//            RestDescription api = discovery.apis().getRest("ml", "v1").execute();
+//            RestMethod method = api.getResources().get("projects").getMethods().get("predict");
+//
+//            JsonSchema param = new JsonSchema();
 
-            JsonSchema param = new JsonSchema();
-
-            param.set("name", String.format("projects/%s/models/%s/versions/%s", projectId, modelId, versionId));
-
-            GenericUrl url = new GenericUrl(UriTemplate.expand(api.getBaseUrl() + method.getPath(), param, true));
-            Log.i("GCloudAPI-url: ", url.toString());
-
+//            param.set("name", String.format("projects/%s/models/%s/versions/%s", projectId, modelId, versionId));
+//
+//            GenericUrl url = new GenericUrl(UriTemplate.expand(api.getBaseUrl() + method.getPath(), param, true));
+//            Log.i("GCloudAPI-url: ", url.toString());
+//
             String contentType = "application/json";
             String jsonInput = "{\"input_values\": [\"" + stockName + "\"]}";
             HttpContent content = new ByteArrayContent(contentType, jsonInput.getBytes());
-            Log.i("GCloudAPI-content len:", "" + content.getLength());
+            Log.i("GCloudAPI-content:", "" + ((ByteArrayContent) content).getInputStream().toString());
+//
+//            GoogleCredential credential = GoogleCredential.getApplicationDefault();
 
-            GoogleCredential credential = GoogleCredential.getApplicationDefault();
-            HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential);
-            HttpRequest request = requestFactory.buildRequest(method.getHttpMethod(), url, content);
+//            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("GCP_Credentials.json"));
+//        InputStream credentialsJSON = GCloudAPI.class.getClassLoader().getResourceAsStream("GCP_Credentials.json");
+//            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsJSON);
+            GoogleCredential credential = new GoogleCredential.Builder()
+                    .setTransport(httpTransport).setJsonFactory(jsonFactory)
+//                    .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+//                    .setServiceAccountScopes(Collections.singleton(STORAGE_SCOPE))
+                    // .setServiceAccountUser(SERVICE_ACCOUNT_EMAIL)
+                    // .setClientSecrets(CLIENT_ID, CLIENT_SECRET)
+                    .build();
+            System.out.println(credential.getServiceAccountId());
 
-            String response = request.execute().parseAsString();
+            HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+//            HttpRequest request = requestFactory.buildRequest(method.getHttpMethod(), url, content);
+//            String response = request.execute().parseAsString();
+            String response = "hi";
             return response;
         } catch (Exception e) {
             Log.e("doInBackground", e.getMessage());
