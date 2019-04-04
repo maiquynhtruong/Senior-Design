ba354d77b2e610405cad145e8a71e73fd8955f13
Mai Truong <maitruong2801@gmail.com>
2019-04-04 12:06:19 -0400

    add feature of show advice button and edit text for stock

--- android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/activities/StockDetails.java
+++ android-stock-advisor/app/src/main/java/com/example/martinruiz/myapplication/activities/StockDetails.java
@@ -2,6 +2,9 @@ package com.example.martinruiz.myapplication.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.martinruiz.myapplication.R;
@@ -15,7 +18,12 @@ public class StockDetails extends AppCompatActivity {
     @BindView(R.id.ticker_symbol) TextView tvTickerSymbol;
     @BindView(R.id.stock_name) TextView tvStockName;
     @BindView(R.id.stock_price) TextView tvStockPrice;
+    @BindView(R.id.user_prediction) EditText etUserPredict;
+    @BindView(R.id.show_advice_btn) Button showAdviceButton;
+    @BindView(R.id.app_prediction) TextView appPrediction;
+    @BindView(R.id.app_prediction_text) TextView appPredictionText;
     private StockQuote stockQuote;
+    static boolean adviceShown = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +32,7 @@ public class StockDetails extends AppCompatActivity {
         ButterKnife.bind(this);
         Bundle bundle = getIntent().getExtras();
         if(! bundle.isEmpty()){ stockQuote = (StockQuote) bundle.getSerializable("stockQuote"); }
-
+        adviceShown = false;
         setCardData();
     }
 
@@ -32,6 +40,22 @@ public class StockDetails extends AppCompatActivity {
         tvTickerSymbol.setText(stockQuote.getStock().getSymbol());
         tvStockName.setText(stockQuote.getStock().getName());
         tvStockPrice.setText(String.format("USD %s", stockQuote.getStock().getPrice()));
-
+        String userPrediction = etUserPredict.getText().toString();
+        showAdviceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (adviceShown) {
+                    appPrediction.setVisibility(View.INVISIBLE);
+                    appPredictionText.setVisibility(View.INVISIBLE);
+                    showAdviceButton.setText("Show Advice");
+                } else {
+                    appPrediction.setVisibility(View.VISIBLE);
+                    appPredictionText.setVisibility(View.VISIBLE);
+                    showAdviceButton.setText("Close Advice");
+                }
+                adviceShown = !adviceShown;
+            }
+        });
     }
+
 }
